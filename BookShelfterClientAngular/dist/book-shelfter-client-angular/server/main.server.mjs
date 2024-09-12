4c2e66574c91f67cc387007228ea252fd33c2ca3
unknown <gorbagor1522@gmail.com>
2024-09-12 14:51:53 +0400

    Added fileReplacements configuration in angular.json for environment-specific API URL management and rebuilt project.

--- BookShelfterClientAngular/dist/book-shelfter-client-angular/server/main.server.mjs
+++ BookShelfterClientAngular/dist/book-shelfter-client-angular/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-MTLEMKXP.mjs";import"./chunk-XKWWWWYZ.mjs";import"./chunk-LTPKZCHS.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-ET7WZ2WY.mjs";import"./chunk-XKWWWWYZ.mjs";import"./chunk-LTPKZCHS.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
