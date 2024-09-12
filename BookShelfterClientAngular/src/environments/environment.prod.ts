d238152770eee0d52419848e0acbf22fcc697864
unknown <gorbagor1522@gmail.com>
2024-09-12 14:31:14 +0400

    Production build updated and dist folder added

--- BookShelfterClientAngular/src/environments/environment.prod.ts
+++ BookShelfterClientAngular/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://bookshelfter152-a4gbbth2c2arf8fn.westeurope-01.azurewebsites.net/api'
+    apiUrl: 'https://bookshelfter152-a4gbbth2c2arf8fn.westeurope-01.azurewebsites.net/api' 
   };
-  
