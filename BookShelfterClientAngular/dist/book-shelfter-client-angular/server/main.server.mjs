dc894e77fde0f84b19e5cca9b39be81c930c773a
unknown <gorbagor1522@gmail.com>
2024-09-14 01:17:17 +0400

    replacement staticwebappa

--- BookShelfterClientAngular/dist/book-shelfter-client-angular/server/main.server.mjs
+++ BookShelfterClientAngular/dist/book-shelfter-client-angular/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-O3AMT2I2.mjs";import"./chunk-W4HIHSZD.mjs";import"./chunk-N7MP7MLP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-24WCUOTQ.mjs";import"./chunk-W4HIHSZD.mjs";import"./chunk-N7MP7MLP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
